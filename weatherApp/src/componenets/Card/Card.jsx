fbbba78f0f6032576a73ec0599cedffdc5a6dc62
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2024-03-05 12:09:27 +0200

    Added some pictures and more info about the weather

--- weatherApp/src/componenets/Card/Card.jsx
+++ weatherApp/src/componenets/Card/Card.jsx
@@ -31,12 +31,15 @@ export default function Card({ searchResult }) {
         }
     }, [cityCode]);
 
+
     return (
         <div className="card">
             <h3>City Name: {city ? city[0].EnglishName : null}</h3>
             <h6>Temp: {weather ? weather[0].Temperature.Metric.Value : null} C</h6>
-            <h6>icon: {weather ? weather[0].WeatherText : null}</h6>
-            <h6>Wind: </h6>
+            <h6>{weather ? weather[0].WeatherText : null}</h6>
+            <h6>Relative Humidity: {weather ? weather[0].RelativeHumidity : null}</h6>
+            <h6>Wind: {weather ? weather[0].Wind.Speed.Metric.Value : null} Km/h</h6>
+            <img src={`../../../public/WeatherPics/${weather ? weather[0].WeatherIcon : null}.jpg`} alt={weather[0].WeatherText} />
         </div>
     );
 }
--- weatherApp/src/services/weatherService.js
+++ weatherApp/src/services/weatherService.js
@@ -2,12 +2,12 @@ const APIKEY = "VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS";
 
 
 export const getCity = (cityName) => {
-    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS&q=${cityName}`)
+    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKEY}&q=${cityName}`)
         .then(res => res.json())
 };
 
 
 export const getWeather = (cityCode) => {
-    return fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS&metric=true`)
+    return fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${APIKEY}&metric=true&details=true`)
         .then(res => res.json())
 };
