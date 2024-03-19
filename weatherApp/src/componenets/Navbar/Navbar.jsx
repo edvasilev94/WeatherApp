06404661f6d24d4b7b8dc491bc5be8c745b849ab
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2024-03-19 12:02:34 +0200

    Added logo to the navbar

--- weatherApp/src/componenets/Navbar/Navbar.jsx
+++ weatherApp/src/componenets/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar () {
 return(
     <nav className="navbar">
         <ul className="navbar-ul">
-            <li className="navbar-li">Logo</li>
+            <li className="navbar-li logo"><img src="../../../logo.png" alt="logo" className="logo" /></li>
             <li className="navbar-li"><Clock /></li>
         </ul>
     </nav>
