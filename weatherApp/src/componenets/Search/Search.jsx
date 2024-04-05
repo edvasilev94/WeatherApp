113959c4e6b9daeabd4bd5a8a7cc55f532629c00
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2024-04-05 10:07:44 +0300

    Refactor the code

--- weatherApp/src/componenets/Search/Search.jsx
+++ weatherApp/src/componenets/Search/Search.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Card from "../Card/Card";
 
+import * as weatherService from "../../services/weatherService"
+
 import "./search.css"
 
 export default function Search() {
@@ -18,29 +20,38 @@ export default function Search() {
     }
 
 
-    const handleChange = async (e) => {
+    const onChangeHandler = async (e) => {
         const inputValue = e.target.value;
         setInput(inputValue);
 
-        const fetchedSuggestions = await fetchSuggestions(inputValue);
-        setSuggestions(fetchedSuggestions);
+        // const fetchedSuggestions = await fetchSuggestions(inputValue);
+        // setSuggestions(fetchedSuggestions);
+        weatherService.fetchSuggestions(inputValue)
+            .then(result => {
+                setSuggestions(result)
+            })
+            .catch (error => {
+                console.error('Error fetching suggestions:', error);
+                return [];
+            })
+
     };
 
-    const fetchSuggestions = async (input) => {
-        const apiKey = 'VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS';
-        const apiUrl = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${input}`;
+    // const fetchSuggestions = async (input) => {
+    //     const apiKey = 'VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS';
+    //     const apiUrl = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${input}`;
 
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error fetching suggestions:', error);
-            return [];
-        }
-    };
+    //     try {
+    //         const response = await fetch(apiUrl);
+    //         const data = await response.json();
+    //         return data;
+    //     } catch (error) {
+    //         console.error('Error fetching suggestions:', error);
+    //         return [];
+    //     }
+    // };
 
-    const handleSelectSuggestion = (suggestion) => {
+    const selectSuggestionHandler = (suggestion) => {
         setInput(suggestion.LocalizedName);
         setSuggestions([]);
     };
@@ -51,7 +62,7 @@ export default function Search() {
             <div className="serach-and-suggestion">
                 <div className="search-bar-container">
                     <div className="serach">
-                        <input className="input-city" type="text" value={input} onChange={handleChange} placeholder="search a city" />
+                        <input className="input-city" type="text" value={input} onChange={onChangeHandler} placeholder="search a city" />
                     </div>
                     <button className="search-button" onClick={onSearchClickHandler}>Search</button>
                 </div>
@@ -59,7 +70,7 @@ export default function Search() {
                 <div className="search-suggestions">
                     <ul className="suggestion-ul">
                         {suggestions.map((suggestion) => (
-                            <li className="suggested-city" key={suggestion.Key} onClick={() => handleSelectSuggestion(suggestion)}>
+                            <li className="suggested-city" key={suggestion.Key} onClick={() => selectSuggestionHandler(suggestion)}>
                                 {suggestion.LocalizedName}, {suggestion.Country.LocalizedName}
                             </li>
                         ))}
--- weatherApp/src/services/weatherService.js
+++ weatherApp/src/services/weatherService.js
@@ -1,6 +1,5 @@
 const APIKEY = "VSY3buYfkcOZGn4jcmEjoRQojV5EwLyS";
 
-
 export const getCity = (cityName) => {
     return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKEY}&q=${cityName}`)
         .then(res => res.json())
@@ -11,3 +10,8 @@ export const getWeather = (cityCode) => {
     return fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${APIKEY}&metric=true&details=true`)
         .then(res => res.json())
 };
+
+export const fetchSuggestions = (input) => {
+    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${APIKEY}&q=${input}`)
+        .then(res => res.json())
+}
